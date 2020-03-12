ead634ac36d485cefa9bc20206cee4d0844e6035
Klimiankou <uklimiankou@users-iMac-8.local>
2020-03-12 18:01:02 +0300

    update styles of word-modal component (landscape mode)

---
+++ src/components/modals/WordModal/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import {red} from '../../../color';
+
+export const modalStyles = StyleSheet.create({
+  contentL: {maxWidth: 500, marginVertical: 30, paddingTop: 40},
+});
+
+export const formStyles = StyleSheet.create({
+  errorTip: {color: red, fontSize: 16},
+  mainFieldsContainerL: {flexDirection: 'row', justifyContent: 'space-between'},
+  mainFieldL: {flex: 0.49},
+});

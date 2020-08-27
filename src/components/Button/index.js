9624b9eb88520157ef8aff1d37e77e3e5ed8fa95
Klimiankou <uklimiankou@users-iMac-8.local>
2020-08-27 13:46:26 +0300

    add icon prop for button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {TouchableOpacity, StyleSheet, Text} from 'react-native';
+import {TouchableOpacity, StyleSheet, Text, Image} from 'react-native';
 import {blue_vivid} from '../../color';
 
 const styles = StyleSheet.create({
-  button: {
-    padding: 12,
-  },
+  button: {padding: 12, flexDirection: 'row', alignItems: 'center'},
   text: {
     fontSize: 17,
     color: blue_vivid,
   },
+  icon: {
+    resizeMode: 'contain',
+    maxWidth: 20,
+    height: 20,
+  },
 });
 
-const Button = ({style = {}, textStyle = {}, onPress, ...props}) => {
+const Button = ({style, textStyle, onPress, ...props}) => {
   return (
     <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
+      {props.icon && (
+        <Image source={props.icon} style={[styles.icon, props.iconStyle]} />
+      )}
       <Text style={[styles.text, textStyle]}>{props.children}</Text>
     </TouchableOpacity>
   );
